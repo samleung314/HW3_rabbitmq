28e5c372f5049bf68c887d3a7c98668f871537ce
samleung314 <samleung314@gmail.com>
2018-03-07 20:17:30 -0500

    rabbitmq server

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ var app = express();
 var port = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+   res.send('Hello! This is HW3 RabbitMQ!');
+ })
+
+app.post('/listen', function (req, res) {
+    res.send(
+        req.body.keys
+    );
+ })
+
+ app.post('/speak', function (req, res) {
+    res.send(
+        req.body.key
+    );
+ })
 
 var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("HW3 RabbitMQ listening at http://%s:%s", host, port)
 })
