e98a84e97d68a9ac9e4cc3e172954cda17f28726
samleung314 <samleung314@gmail.com>
2018-03-07 20:02:56 -0500

    rabbitmq server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-// server.js
-var app = require('./app');
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 80;
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
