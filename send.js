013f7c93ac6c070256837d496e1a2fbcb7972ab5
samleung314 <samleung314@gmail.com>
2018-03-07 22:01:39 -0500

    setting up exclusive queue

--- send.js
+++ send.js
@@ -2,12 +2,12 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'messages';
+    var exchange = 'hw3';
     var msg = 'Hello World!';
 
-    ch.assertQueue(q, {durable: false});
+    ch.assertQueue(exchange, {durable: false});
     // Note: on Node 6 Buffer.from(msg) should be used
-    ch.sendToQueue(q, new Buffer(msg));
+    ch.sendToQueue(exchange, new Buffer(msg));
     console.log(" [x] Sent %s", msg);
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
