231041796e5aa877b535fbd469e0cc80bac78149
samleung314 <samleung314@gmail.com>
2018-03-08 00:36:19 -0500

    setting up exclusive queue

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function speak(key, msg) {
 
 app.post('/speak', function (req, res) {
   console.log("Key: " + req.body.key.toString() + " Msg: " + req.body.msg.toString());
-  res.status(200);
+  res.send(200);
   //speak(req.body.key.toString(), req.body.msg.toString());
 })
 
