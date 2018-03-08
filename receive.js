1876bed812c031d7cf2b25fdff48a7349afc1491
samleung314 <samleung314@gmail.com>
2018-03-07 21:57:26 -0500

    setting up exclusive queue

--- receive.js
+++ receive.js
@@ -3,13 +3,13 @@ var amqp = require('amqplib/callback_api');
 var keys = ['a','b','c','d','e']
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'messages';
+    var exchange = 'hw3';
 
     ch.assertQueue('', {exclusive: true}, function(err, que) {
       console.log(' [*] Waiting for messages. To exit press CTRL+C');
 
       keys.forEach(function(key) {
-        ch.bindQueue(que.queue, q, key);
+        ch.bindQueue(que.queue, exchange, key);
       });
 
       ch.consume(que.queue, function(msg) {
--- send.js
+++ send.js
@@ -2,7 +2,7 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'messages';
     var msg = 'Hello World!';
 
     ch.assertQueue(q, {durable: false});
