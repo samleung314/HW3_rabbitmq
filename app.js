8f8c9f0339c950958d6faecb050fa148e37037f8
samleung314 <samleung314@gmail.com>
2018-03-07 22:05:17 -0500

    setting up exclusive queue

--- app.js
+++ app.js
@@ -6,19 +6,41 @@ var bodyParser = require('body-parser');
 //create express app
 var app = express();
 
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.send('Hello! This is HW3 RabbitMQ!');
-  })
+  res.send('Hello! This is HW3 RabbitMQ!');
+})
 
 app.post('/listen', function (req, res) {
-    console.log("(/listen) Keys: " + req.body.keys);
-  })
- 
-app.post('/speak', function (req, res) {
-    console.log("(/speak) Key: " + req.body.key + " Msg: " + req.body.msg);
-  })
+  var keys = req.body.keys
+  console.log("(/listen) Keys: " + keys);
 
-  module.exports = app;
+  keys = keys.split(',');
+
+  var amqp = require('amqplib/callback_api');
+  amqp.connect('amqp://localhost', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+      var exchange = 'hw3';
+
+      ch.assertQueue('', { exclusive: true }, function (err, que) {
+        console.log(' [*] Waiting for messages. To exit press CTRL+C');
+
+        keys.forEach(function (key) {
+          ch.bindQueue(que.queue, exchange, key);
+        });
+
+        ch.consume(que.queue, function (msg) {
+          console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+        }, { noAck: true });
+      });
+    });
+  });
+})
+
+app.post('/speak', function (req, res) {
+  console.log("(/speak) Key: " + req.body.key + " Msg: " + req.body.msg);
+})
+
+module.exports = app;
