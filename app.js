0ea1fb0c3ab939476249889a28d96e912c35bdd8
samleung314 <samleung314@gmail.com>
2018-03-07 21:26:58 -0500

    setting up exclusive queue

--- app.js
+++ app.js
@@ -14,27 +14,11 @@ app.get('/', function (req, res) {
   })
  
  app.post('/listen', function (req, res) {
-     console.log("Keys: " + req.body.keys);
+     console.log("(/listen) Keys: " + req.body.keys);
   })
  
  app.post('/speak', function (req, res) {
-     console.log("Key: " + req.body.key);
-     console.log("Msg: " + req.body.msg);
+     console.log("(/speak) Key: " + req.body.key + "Msg: " + req.body.msg);
   })
-
-  var amqp = require('amqplib/callback_api');
-
-  amqp.connect('amqp://localhost', function(err, conn) {
-    conn.createChannel(function(err, ch) {
-      var q = 'hello';
-      var msg = 'Hello World!';
-  
-      ch.assertQueue(q, {durable: false});
-      // Note: on Node 6 Buffer.from(msg) should be used
-      ch.sendToQueue(q, new Buffer(msg));
-      console.log(" [x] Sent %s", msg);
-    });
-    setTimeout(function() { conn.close(); process.exit(0) }, 500);
-  });
 
   module.exports = app;
--- receive.js
+++ receive.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 
-var amqp = require('./node_modules/amqplib/callback_api');
+var amqp = require('amqplib/callback_api');
+
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+  console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+  process.exit(1);
+}
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var ex = 'direct_logs';
 
-    ch.assertQueue(q, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+    ch.assertExchange(ex, 'direct', {durable: false});
+
+    ch.assertQueue('', {exclusive: true}, function(err, q) {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+      args.forEach(function(severity) {
+        ch.bindQueue(q.queue, ex, severity);
+      });
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+      }, {noAck: true});
+    });
   });
 });
