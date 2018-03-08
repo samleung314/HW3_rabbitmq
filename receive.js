32527bd685a57fbc7a47baa54303634acb66e60d
samleung314 <samleung314@gmail.com>
2018-03-07 21:54:09 -0500

    setting up exclusive queue

--- receive.js
+++ receive.js
@@ -2,12 +2,18 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'messages';
 
-    ch.assertQueue(q, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+    ch.assertQueue('', {exclusive: true}, function(err, que) {
+      console.log(' [*] Waiting for messages. To exit press CTRL+C');
+
+      args.forEach(function(key) {
+        ch.bindQueue(que.queue, q, key);
+      });
+
+      ch.consume(que.queue, function(msg) {
+        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+      }, {noAck: true});
+    });
   });
 });
