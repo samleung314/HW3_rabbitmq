b06175c36e7aa9314e10ae3ee9f463c9a0c96e0a
samleung314 <samleung314@gmail.com>
2018-03-08 01:00:20 -0500

    testing exchanges, trying to get /listen to work

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function listen(keys) {
+function listen(keys, res) {
   var amqp = require('amqplib/callback_api');
   amqp.connect('amqp://localhost', function (err, conn) {
     conn.createChannel(function (err, ch) {
@@ -38,7 +38,7 @@ function listen(keys) {
 
 app.post('/listen', function (req, res) {
   console.log("(/listen) Keys: " + req.body.keys);
-  listen(req.body.keys.toString().split(','));
+  listen(req.body.keys.toString().split(','), res);
 })
 
 function speak(key, msg) {
