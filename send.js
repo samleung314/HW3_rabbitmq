f87de6282d627a22e2617e37777fc67505696146
samleung314 <samleung314@gmail.com>
2018-03-07 21:43:42 -0500

    setting up exclusive queue

--- send.js
+++ send.js
@@ -1,6 +1,6 @@
 
 
-var amqp = require('/amqplib/callback_api');
+var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
